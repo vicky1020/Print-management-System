b8778cc46a32480bed3b7a08ce9626ca958c2bbf
rakshith076 <rakshith@vuedata.com>
2017-10-26 15:44:22 +0530

    UI restructuring and AutoMapper implementation

--- Common/Mapping/Mapper.cs
+++ Common/Mapping/Mapper.cs
@@ -5,59 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using EF;
 using PrintManagement.Common.Models;
+using AutoMapper;
 
 namespace PrintManagement.Common.Mapping
 {
-    public static class Mapper
+    public static class Mapping
     {
         #region Entity To Model Mapping
-        
+        static Mapping()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<OrderItem, OrderItemModel>();
+
+            });
+        }
        
         public static UsersModel ToUserModel(this User user)
         {
             return new UsersModel
             {
-               
                 UserName = user.UserName,
                 Password = user.Password
-               
-                
-            
-
-
             };
         }
         public static OrderItemModel ToOrderModel(this OrderItem item)
         {
-            return new OrderItemModel
-            {
-                OrderId = item.OrderId,
-                Active = item.Active,
-                AdditionalPaperCount = item.AdditionalPaperCount,
-                AdditionalPaperSize = item.AdditionalPaperSize,
-                AdditonalPaperGSM = item.AdditonalPaperGSM,
-                Amount=item.Amount,
-                CreatedBy=item.CreatedBy,
-                CreatedDate=item.CreatedDate,
-               JobStatus=item.JobStatus,
-               LedgerFalio=item.LedgerFalio,
-               PaperColor=item.PaperColor,
-               PaperGSM=item.PaperGSM,
-               PaperQuality=item.PaperQuality,
-               PaperSides=item.PaperSides,
-               PaperSize=item.PaperSize,
-               ProductItemId=item.ProductItemId,
-              Quantity=item.Quantity  
-            };
+            OrderItemModel customerViewItem =
+              Mapper.Map<OrderItem, OrderItemModel>(item);
+            return customerViewItem;
         }
         #endregion
 
         #region Model to Entity Mapping
 
-
-        #endregion
-
-        #region Private Methods
 
         #endregion
     }
